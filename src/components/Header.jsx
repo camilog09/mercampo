585ca57afb7156e41bfe12a3f5081a8af624d999
camilog09 <camilo.gaitan09@gmail.com>
2023-07-22 19:35:11 -0500

    Header v0.1

---
+++ src/components/Header.jsx
@@ -0,0 +1,108 @@
+import React, { useContext } from "react";
+import styles from "../styles/Header.module.css";
+import { Link } from "react-router-dom";
+import Menu from "./Menu";
+import AppContext from "../context/AppContext";
+
+import logo from "../assets/logomercampo.svg";
+import shoppingCart from "../assets/icons/shopping-cart.svg";
+import menu from "../assets/icons/menu.svg";
+
+const Header = () => {
+  const { state, toggleOrder, toggleMenu } = useContext(AppContext);
+
+  return (
+    <>
+      <nav className={styles.Nav}>
+        <div className={styles["navbar-left"]}>
+          <img src={menu} alt="menu" className={styles.menu} />
+          <Link href="/">
+            <img src={logo} alt="logo" className={styles["nav-logo"]} />
+          </Link>
+        </div>
+        <div className={styles["navbar-center"]}>
+          <input
+            type="text"
+            placeholder="Buscar..."
+            className={styles["nav-search"]}
+          />
+        </div>
+        <div className={styles["navbar-right"]}>
+          <ul>
+            <li
+              className={styles["navbar-shopping-cart"]}
+              onClick={() => toggleOrder()}
+              aria-hidden="true"
+            >
+              <img
+                className={(styles["more-clickable-area"], styles.pointer)}
+                src={shoppingCart}
+                alt="shopping cart"
+              />
+              {state.cart.length > 0 ? <div>{state.cart.length}</div> : null}
+            </li>
+          </ul>
+        </div>
+      </nav>
+      {/* <nav className={styles.Nav}>
+        <img src={menu} alt="menu" className={styles.menu} />
+        <div className={styles["navbar-left"]}>
+          <Link href="/">
+            <img src={logo} alt="logo" className={styles["nav-logo"]} />
+          </Link>
+          <ul>
+            <li>
+              <Link href="/">Info</Link>
+            </li>
+            <li>
+              <Link href="/">Clothes</Link>
+            </li>
+            <li>
+              <Link href="/">Electronics</Link>
+            </li>
+            <li>
+              <Link href="/">Furnitures</Link>
+            </li>
+            <li>
+              <Link href="/">Toys</Link>
+            </li>
+            <li>
+              <Link href="/">Others</Link>
+            </li>
+          </ul>
+        </div>
+        <div className={styles["navbar-right"]}>
+          <ul>
+            <li
+              className={
+                (styles["more-clickable-area"],
+                styles["navbar-email"],
+                styles.pointer)
+              }
+              onClick={() => toggleMenu()}
+              aria-hidden="true"
+            >
+              platzi@example.com
+            </li>
+            <li
+              className={styles["navbar-shopping-cart"]}
+              onClick={() => toggleOrder()}
+              aria-hidden="true"
+            >
+              <img
+                className={(styles["more-clickable-area"], styles.pointer)}
+                src={shoppingCart}
+                alt="shopping cart"
+              />
+              {state.cart.length > 0 ? <div>{state.cart.length}</div> : null}
+            </li>
+          </ul>
+        </div>
+        {state.menuIsOpen && <Menu />}
+      </nav>
+      {state.orderIsOpen && <MyOrder />} */}
+    </>
+  );
+};
+
+export default Header;
